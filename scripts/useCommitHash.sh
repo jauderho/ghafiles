fb8f547e5b3ff30b53ffdab559c3c687569c3b9b
Jauder Ho <jauderho@users.noreply.github.com>
2022-05-05 19:43:39 +0000

    Update useCommitHash.sh
    Signed-off-by: Jauder Ho <jauderho@users.noreply.github.com>

--- scripts/useCommitHash.sh
+++ scripts/useCommitHash.sh
@@ -13,8 +13,7 @@ IFS=$'\n\t'
 git pull
 
 # initial conversion to commit hashes
-sed -i  \
-	-e 's/docker\/login-action@v1.12.0/docker\/login-action@42d299face0c5c43a0487c477f595ac9cf22f1a7 # v1.12.0/' \
+sed -i -e 's/docker\/login-action@v1.12.0/docker\/login-action@42d299face0c5c43a0487c477f595ac9cf22f1a7 # v1.12.0/' \
 	-e 's/docker\/metadata-action@v3/docker\/metadata-action@e5622373a38e60fb6d795a4421e56882f2d7a681 # v3/' \
 	-e 's/docker\/setup-buildx-action@v1/docker\/setup-buildx-action@94ab11c41e45d028884a99163086648e898eed25 # v1/' \
 	-e 's/docker\/setup-qemu-action@v1.2.0/docker\/setup-qemu-action@27d0a4f181a40b142cce983c5393082c365d1480 # v1.2.0/' \
@@ -28,8 +27,7 @@ sed -i  \
 	.github/workflows/*.yml
 
 # bulk updates (2nd pass)
-sed -i  \		
-	-e 's/docker\/build-push-action@ac9327eae2b366085ac7f6a2d02df8aa8ead720a/docker\/build-push-action@e551b19e49efd4e98792db7592c17c09b89db8d8/' \
+sed -i -e 's/docker\/build-push-action@ac9327eae2b366085ac7f6a2d02df8aa8ead720a/docker\/build-push-action@e551b19e49efd4e98792db7592c17c09b89db8d8/' \
 	-e 's/docker\/login-action@dd4fa0671be5250ee6f50aedf4cb05514abda2c7/docker\/login-action@49ed152c8eca782a232dede0303416e8f356c37b/' \
 	-e 's/docker\/metadata-action@b2391d37b4157fa4aa2e118d643f417910ff3242/docker\/metadata-action@69f6fc9d46f2f8bf0d5491e4aabe0bb8c6a4678a/' \
 	-e 's/docker\/setup-buildx-action@f211e3e9ded2d9377c8cadc4489a4e38014bc4c9/docker\/setup-buildx-action@dc7b9719a96d48369863986a06765841d7ea23f6/' \
